8455be40da0c72eba059152309c9486eb53f0bb1
K-Haseul <yeounkkyugwi@gmail.com>
2023-01-13 20:54:12 +0900

    function

--- Day 4/function.py
+++ Day 4/function.py
@@ -19,7 +19,6 @@ def 함수이름(입력):
 def 함수이름(입력):
     실행할 코드 작성
     return 출력
-'''
 
 def 하율():
     print("하율이")
@@ -42,3 +41,38 @@ def 서현(입력):
 print(서현(5))
 결과=서현(7)
 print(결과)
+'''
+#1
+def 함수(몇번):
+    for i in range(몇번):
+        print("파이썬 함수 배움")
+함수(5)
+
+#2
+def 곱하기(a,b):
+    print(a*b)
+    return a*b
+곱하기(2,3)
+곱하기(10,20)
+
+#2-1
+def 삼각형넓이(a,b):
+    print(a*b/2)
+    return a*b/2
+삼각형넓이(10,5)
+삼각형넓이(6,8)
+
+#2-2
+def 구구단(a):
+    for i in range(1,10):
+        print(a*i)
+구구단(3)
+구구단(7)
+
+#2-3
+def 홀수(a):
+    for i in range(a*2):
+        if i%2==1:
+            print(i)
+홀수(5)
+홀수(6)
