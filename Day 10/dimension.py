7112686ccab5ef198c993c50997c2a1d11b50122
K-Haseul <yeounkkyugwi@gmail.com>
2023-03-08 22:36:48 +0900

    묭

---
+++ Day 10/dimension.py
@@ -0,0 +1,53 @@
+import numpy as np
+import matplotlib.image as img
+import matplotlib.pyplot as plt
+
+a=np.array([1,2,3])
+b=np.array([
+    [1,2],
+    [3,4]
+])
+c=np.array([
+    [[1,2],[3,4]],
+    [[1,2],[3,4]]
+])
+d=np.array([
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+])
+
+# print(a)
+# print(b)
+# print(c)
+# print(a.size)
+# print(b.size)
+# print(c.size)
+# print(a.shape)
+# print(b.shape)
+# print(c.shape)
+# print(a[:2])
+# print(a[1:])
+# a[0]=100
+# a[1:]=200
+# a[:]=0
+# print(a)
+# b[:]=0
+# b[1,0]=1
+# d[:]=0
+# d[0,0]=1
+# d[0,2]=1
+# d[1,1]=1
+# d[2,0]=1
+# d[2,2]=1
+# d[1,:]=1
+# d[0,:]=0
+# d[2,:]=0
+# d[2,:]=0
+# d[:,0]=1
+# d[:,1]=0
+# d[:,2]=1
+# d[:]=0
+# d[1:,1:]=1
+plt.imshow(e)
+plt.show()
